b29f4d71090e6d9bb371c02f8c16d696b5461f05
astroboy107 <gerardoriosjr@gmail.com>
2013-11-14 10:10:43 -0800

    Signed-off-by: astroboy107 <gerardoriosjr@gmail.com>

--- cr2490090/Array_Manipulation_111213/main.cpp
+++ cr2490090/Array_Manipulation_111213/main.cpp
@@ -3,7 +3,7 @@ Caleb Rosenquist
 November 12th, 2013
 Purpose: To locate a specific element in an array and delete it shit
 
-will this change? I DUNNO
+will this change? I DUNNO wat to do
 */
 
 //Libraries
