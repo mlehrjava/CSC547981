7f1151936cca696ab54ce772d54a8870d7964fc3
mlehrjava <mlehrjava@earthlink.net>
2013-11-21 10:21:28 -0800

    Signed-off-by: mlehrjava <mlehrjava@earthlink.net>

--- ml1150258/MarkSortDB/main.cpp
+++ ml1150258/MarkSortDB/main.cpp
@@ -13,12 +13,10 @@ using namespace std;
 //Global Constants
 
 //Function Prototypes
-void filArry(int [],int);
+void filArry(int [],int [],int);
 void prntAry(int [],int,int);
-void swap1(int &,int &);
-void swap2(int &,int &);
-void swap3(int [],int,int);
-void copy(int [],int [],int);
+void prntAry(int [],int [],int,int);
+void swap(int &,int &);
 void sortPos(int [],int,int);
 void markSort(int [],int);
 
@@ -26,18 +24,18 @@ int main(int argc, char *argv[]){
     //Declare variables and initialize the
     //random number seed
     const int SIZE=100;
-    int array[SIZE],acopy[SIZE];
+    int array[SIZE],indx[SIZE];
     srand(static_cast<unsigned int>(time(0)));
     //Fill the array
-    filArry(array,SIZE);
-    //Copy the array
-    copy(array,acopy,SIZE);
+    filArry(array,indx,SIZE);
     //Print the array
     prntAry(array,SIZE,10);
+    prntAry(indx,SIZE,10);
+    prntAry(array,indx,SIZE,10);
     //Sort the array
-    markSort(acopy,SIZE);
+    //markSort(acopy,SIZE);
     //Print the sorted array
-    prntAry(acopy,SIZE,10);
+    //prntAry(acopy,SIZE,10);
     //Exit stage right
     return EXIT_SUCCESS;
 }
@@ -51,34 +49,24 @@ void markSort(int a[],int n){
 void sortPos(int a[],int n,int pos){
      if(pos>n-1)return;
      for(int i=pos+1;i<n;i++){
-          //if(a[pos]>a[i])swap1(a[pos],a[i]);
-          //if(a[pos]>a[i])swap2(a[pos],a[i]);
-          if(a[pos]>a[i])swap3(a,pos,i);
+          if(a[pos]>a[i])swap(a[pos],a[i]);
      }
 }
 
-void copy(int a[],int b[],int n){
-     for(int i=0;i<n;i++){
-          b[i]=a[i];
-     }
-}
 
-void swap3(int a[],int pos1,int pos2){
-     int temp=a[pos1];
-     a[pos1]=a[pos2];
-     a[pos2]=temp;
-}
-
-void swap2(int &a,int &b){
-     a=a^b;
-     b=a^b;
-     a=a^b;
-}
-
-void swap1(int &a,int &b){
+void swap(int &a,int &b){
      int temp=a;
      a=b;
      b=temp;
+}
+
+void prntAry(int a[],int indx[],int n,int perLine){
+     cout<<endl;
+     for(int i=0;i<n;i++){
+          cout<<a[indx[i]]<<" ";
+          if(i%perLine==(perLine-1))cout<<endl;
+     }
+     cout<<endl;
 }
 
 void prntAry(int a[],int n,int perLine){
@@ -90,8 +78,9 @@ void prntAry(int a[],int n,int perLine){
      cout<<endl;
 }
 
-void filArry(int a[],int n){
+void filArry(int a[],int indx[],int n){
      for(int i=0;i<n;i++){
           a[i]=rand()%900+100;
+          indx[i]=i;
      }
 }
