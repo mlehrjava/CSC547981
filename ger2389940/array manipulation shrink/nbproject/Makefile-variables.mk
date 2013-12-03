776bcadae8b2d63b1acb63fe426ac025f31fe15c
astroboy107 <gerardoriosjr@gmail.com>
2013-12-03 10:53:31 -0800

    Signed-off-by: astroboy107 <gerardoriosjr@gmail.com>

---
+++ ger2389940/array manipulation shrink/nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=Cygwin_4.x-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin_4.x-Windows
+CND_ARTIFACT_NAME_Debug=array_manipulation
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/array_manipulation
+CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin_4.x-Windows/package
+CND_PACKAGE_NAME_Debug=arraymanipulation.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/package/arraymanipulation.tar
+# Release configuration
+CND_PLATFORM_Release=Cygwin_4.x-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/Cygwin_4.x-Windows
+CND_ARTIFACT_NAME_Release=array_manipulation
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin_4.x-Windows/array_manipulation
+CND_PACKAGE_DIR_Release=dist/Release/Cygwin_4.x-Windows/package
+CND_PACKAGE_NAME_Release=arraymanipulation.tar
+CND_PACKAGE_PATH_Release=dist/Release/Cygwin_4.x-Windows/package/arraymanipulation.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
